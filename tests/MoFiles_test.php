46149e1d4b251dab32f000870702f4bed5014c04
Michal Čihař <michal@cihar.com>
2016-02-22 16:49:24 +0100

    Test basic files loading
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ tests/MoFiles_test.php
@@ -0,0 +1,35 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Test for MO files parsing.
+ *
+ * @package PhpMyAdmin-test
+ */
+
+require_once 'src/streams.php';
+
+class MoFileTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideMoFiles
+     */
+    public function testMoFile($filename)
+    {
+        $reader = new FileReader($filename);
+        $parser = new MoTranslator\MoTranslator($reader);
+        $parser->load_tables();
+        $this->assertEquals(
+            $parser->translate('Column'),
+            'Pole'
+        );
+    }
+
+    public function provideMoFiles()
+    {
+        $result = array();
+        foreach (glob('./tests/data/*.mo') as $file) {
+            $result[] = array($file);
+        }
+        return $result;
+    }
+}
