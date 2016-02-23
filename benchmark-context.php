d176ae525da5f8153cd77c86e690e4f18a7970c4
Michal Čihař <michal@cihar.com>
2016-02-23 11:19:37 +0100

    Add benchmark for pgettext
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ benchmark-context.php
@@ -0,0 +1,23 @@
+<?php
+
+require './vendor/autoload.php';
+
+$files = array('./tests/data/big.mo', './tests/data/little.mo');
+
+$start = microtime(true);
+
+foreach ($files as $filename) {
+    $parser = new MoTranslator\MoTranslator($filename);
+    for ($i = 0; $i < 200000; $i++) {
+        $parser->pgettext(
+            'Display format',
+            'Table'
+        );
+    }
+}
+
+$end = microtime(true);
+
+$diff = $end - $start;
+
+echo "Execution took $diff seconds\n";
