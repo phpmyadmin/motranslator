1068f0508c7ced565c3151a4b70322550b7f9fe5
Michal Čihař <michal@cihar.com>
2016-02-23 10:28:34 +0100

    Split out plurals test
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- tests/Parsing_test.php
+++ tests/Parsing_test.php
@@ -52,41 +52,4 @@ class ParsingTest extends PHPUnit_Framework_TestCase
             ),
         );
     }
-
-    /**
-     * Test for npgettext
-     *
-     * @param int    $number   Number
-     * @param string $expected Expected output
-     *
-     * @return void
-     *
-     * @dataProvider data_provider_test_npgettext
-     */
-    public function test_npgettext($number, $expected)
-    {
-        $parser = new MoTranslator\MoTranslator(null);
-        $result = $parser->npgettext(
-            "context",
-            "%d pig went to the market\n",
-            "%d pigs went to the market\n",
-            $number
-        );
-        $this->assertSame($expected, $result);
-    }
-
-    /**
-     * Data provider for test_npgettext
-     *
-     * @return array
-     */
-    public static function data_provider_test_npgettext()
-    {
-        return array(
-            array(1, "%d pig went to the market\n"),
-            array(2, "%d pigs went to the market\n"),
-        );
-    }
-
 }
-
---
+++ tests/Plural_test.php
@@ -0,0 +1,45 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Test for gettext parsing.
+ *
+ * @package PhpMyAdmin-test
+ */
+
+class PluralTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test for npgettext
+     *
+     * @param int    $number   Number
+     * @param string $expected Expected output
+     *
+     * @return void
+     *
+     * @dataProvider data_provider_test_npgettext
+     */
+    public function test_npgettext($number, $expected)
+    {
+        $parser = new MoTranslator\MoTranslator(null);
+        $result = $parser->npgettext(
+            "context",
+            "%d pig went to the market\n",
+            "%d pigs went to the market\n",
+            $number
+        );
+        $this->assertSame($expected, $result);
+    }
+
+    /**
+     * Data provider for test_npgettext
+     *
+     * @return array
+     */
+    public static function data_provider_test_npgettext()
+    {
+        return array(
+            array(1, "%d pig went to the market\n"),
+            array(2, "%d pigs went to the market\n"),
+        );
+    }
+}
