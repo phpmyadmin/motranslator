b1006611404e9fd3f2daa3329047ef0aa30b00df
Michal Čihař <michal@cihar.com>
2016-02-24 13:07:12 +0100

    Adjust benchmarks to new class names
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- benchmark.php
+++ benchmark.php
@@ -8,7 +8,7 @@ $start = microtime(true);
 
 for ($i = 0; $i < 200; $i++) {
     foreach ($files as $filename) {
-        $parser = new MoTranslator\MoTranslator($filename);
+        $parser = new MoTranslator\Translator($filename);
         $parser->gettext('Column');
     }
 }
