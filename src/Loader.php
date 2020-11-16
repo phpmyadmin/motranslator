58a943096734ce793c970d6b3f56ee894139cea1
William Desportes <williamdes@wdes.fr>
2020-11-16 17:03:47 +0100

    Add spaces to fix phpcs
    phpcs: SlevomatCodingStandard.Namespaces.NamespaceSpacing.IncorrectLinesCountBeforeNamespace
    
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- src/Loader.php
+++ src/Loader.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
+
 namespace PhpMyAdmin\MoTranslator;
 
 use function array_push;
--- src/StringReader.php
+++ src/StringReader.php
@@ -22,6 +22,7 @@ declare(strict_types=1);
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
+
 namespace PhpMyAdmin\MoTranslator;
 
 use const PHP_INT_MAX;
